0b8ba6e13b9b65f42ff6c8bacffd2b732e589a41
MalphasWats <malphas@subdimension.co.uk>
2013-04-27 07:51:26 +0100

    Initial files

---
+++ game.js
@@ -0,0 +1,21 @@
+/* MalphasWats - Ludum Dare 26 */
+
+function Game()
+{
+
+    this.setup = function()
+    {
+    
+    }
+    
+    this.update = function()
+    {
+    
+    }
+    
+    this.draw = function()
+    {
+    
+    }
+
+}
